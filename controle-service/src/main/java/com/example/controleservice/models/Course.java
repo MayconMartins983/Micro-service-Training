2df52429f78669d753e6967144ff328c469bdd79
Maycon Martins <maycon.rocha2100@gmail.com>
2022-12-25 20:45:12 -0300

    adiciona auth jwt

--- controle-service/src/main/java/com/example/controleservice/models/Course.java
+++ controle-service/src/main/java/com/example/controleservice/models/Course.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @Data
